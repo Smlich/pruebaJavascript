7bc15454577c3e3adf44631e9031d70e20627290
smlich <facudecano09@gmail.com>
2023-03-06 06:43:36 -0300

    agrego ejercicio 7

--- ejercicios/6.js
+++ ejercicios/6.js
@@ -2,6 +2,9 @@ let numeroA = parseInt(prompt('Ingrese un numero'));
 let numeroB = parseInt(prompt('Ingrese otro numero'));
 if (numeroA > numeroB) {
     document.write ('El numero mayor es ' + numeroA)
-}else {
+}else if (numeroB > numeroA){
     document.write ('El numero mayor es ' + numeroB)
-}
+}else{
+    document.write ('Ambos numeros son iguales')
+} 
+    
---
+++ ejercicios/7.js
@@ -0,0 +1,12 @@
+let numeroA = parseInt(prompt('Ingrese un numero'));
+let numeroB = parseInt(prompt('Ingrese otro numero'));
+let numeroC = parseInt(prompt('Ingrese otro numero'));
+if (numeroA > numeroB && numeroA > numeroC ) {
+    document.write ('El numero mayor es ' + numeroA)
+}else if (numeroB > numeroC){
+    document.write ('El numero mayor es ' + numeroB)
+}else if (numeroC > numeroB){
+    document.write ('El numero mayor es ' + numeroC)
+}else{
+    document.write ('Los tres numeros son iguales')
+}
