de1f650a0be8683cd14f6926c2abb420e6c8674f
smlich <facudecano09@gmail.com>
2023-03-06 06:21:33 -0300

    agrego ejercicio 6

---
+++ ejercicios/6.js
@@ -0,0 +1,7 @@
+let numeroA = parseInt(prompt('Ingrese un numero'));
+let numeroB = parseInt(prompt('Ingrese otro numero'));
+if (numeroA > numeroB) {
+    document.write ('El numero mayor es ' + numeroA)
+}else {
+    document.write ('El numero mayor es ' + numeroB)
+}
