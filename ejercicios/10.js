7073f90f252da4b639d6cc03db4183bb61fd188d
smlich <facudecano09@gmail.com>
2023-03-06 08:06:03 -0300

    agrego ejercicio 10

---
+++ ejercicios/10.js
@@ -0,0 +1,6 @@
+let numero = parseInt(prompt('Ingrese un numero'))
+if (numero % 2 === 0 || numero % 3 === 0 || numero % 5 === 0 || numero % 7 === 0){
+    document.write ('si es divisible por 2,3,5 o 7')
+}else{
+    document.write ('no es divisible por 2,3,5 ni 7')
+} 
