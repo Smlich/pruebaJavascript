b8157e46b5ba347810421fa076db305eabb368d2
smlich <facudecano09@gmail.com>
2023-04-04 13:40:23 -0300

    agreco correciones punto 7 y 10

--- ejerciciosTP4/7.js
+++ ejerciciosTP4/7.js
@@ -22,9 +22,22 @@ class Agenda {
     añadirContacto(contacto){
         if(this.#contactos.length < this.#tamanio){
             this.#contactos.push(contacto) 
-        }else[
+        }else{
             document.write('La agenda esta llena, no puede agregar más contactos. <br/>')
-        ]
+        }
+    }
+    existeContacto(contactoBuscado){
+        let contactosEncontrados = 0
+        for(let i = 0;i < this.#contactos.length; i++){
+            if(this.#contactos[i].nombre.includes(contactoBuscado)){
+                document.write('Nombre del contacto: '+this.#contactos[i].nombre+ '<br/>' +'Numero: '+this.#contactos[i].numero +'<br/><hr/>');
+                contactosEncontrados++;
+            }
+        }
+        if (contactosEncontrados == 0){
+            document.write('No encontramos el contacto que esta buscando.')
+        }
+        
     }
     listarContactos(){
         document.write('<table class="table"><thead><tr><th>Nombre</th><th>Numero</th></tr></thead><tbody>')
@@ -33,6 +46,21 @@ class Agenda {
         document.write('<tr><td>' + contacto.nombre + '</td><td>' + contacto.numero + '</td>')
     }
     document.write('</tbody></table>')
+    }
+    agendaLlena(){
+        if(this.#contactos.length == this.#tamanio){
+            document.write('La agenda esta llena, no puede agregar más contactos. <br/>')
+        }else{
+            document.write('La agenda esta no esta llena, se puede agregar más contactos. <br/>')
+        }
+        
+    }
+    huecosLibres(){
+        if(this.#contactos.length < this.#tamanio){
+        document.write('Quedan '+ (this.#tamanio - this.#contactos.length) + ' espacios libres. <br/>')
+        }else{
+            document.write('La agenda esta llena, no puede agregar más contactos. <br/>')
+        }
     }
 }
 class Contacto{
@@ -60,10 +88,13 @@ class Contacto{
     }
 }
 let agenda1 =new Agenda(2)
-const Juan = new Contacto('Juan Antonio','3816168898')
+const contacto1 = new Contacto('Juan Antonio','3816168898')
+const contacto2 = new Contacto('Domingo Sangucheria','3816168888')
+agenda1.añadirContacto(contacto1)
 
-agenda1.añadirContacto(Juan)
-agenda1.añadirContacto(Juan)
-agenda1.añadirContacto(Juan)
-console.log(agenda1.contactos)
 agenda1.listarContactos()
+console.log(agenda1.contactos)
+
+agenda1.existeContacto('Juan')
+agenda1.huecosLibres()
+agenda1.agendaLlena()
